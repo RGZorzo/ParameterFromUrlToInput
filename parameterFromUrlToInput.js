02975f248676a7643ba1504a2f3544d124213307
Ricardo Zorzo <rgarciazorzo@gmail.com>
2019-04-10 11:11:58 +0200

    Create parameterFromUrlToInput.js

---
+++ parameterFromUrlToInput.js
@@ -0,0 +1,22 @@
+ jQuery(document).ready(function(){
+	if(jQuery('#traffic-source-form-input').length){
+		var trafficSourceParameter = GetURLParameter('p');
+		if(trafficSourceParameter !== false){
+			jQuery('#traffic-source-form-input').val(trafficSourceParameter);
+		}
+	}
+});
+
+function GetURLParameter(sParam) {
+
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	
+	for (var i = 0; i < sURLVariables.length; i++) {
+		var sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] == sParam) {
+			return sParameterName[1];
+		}
+	}
+	return false;
+}
